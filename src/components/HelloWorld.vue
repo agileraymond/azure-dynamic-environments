8d335f898414b07d0dc623fb9f7afbdc4e3d1cdf
Raymond Sanchez <agileRaymond@gmail.com>
2021-04-17 13:12:04 -0500

    initial commit for azure dynamic environments

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,55 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{ msg }}</h1>    
   </div>
+  <div>
+    <span>Environment name:</span>
+    <input type="text" /> 
+  </div>
+  <div>
+    <span>Location:</span>
+    <select>
+      <option value="centralus">central us</option>
+      <option value="eastus">east us</option>
+      <option value="westus">west us</option>
+    </select> 
+  </div>
+  <div>
+    <input type="button" value="Create Environment" v-on:click="createOrUpdateEnvironment" /> 
+  </div>
+    
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+  },
+  data() {
+    return {
+        subscriptionId: process.env.VUE_APP_SUBSCRIPTION_ID,
+    }
+  }, 
+  mounted () {
+    // GET 
+    axios
+      .get(`https://management.azure.com/subscriptions/${this.subscriptionId}/resourcegroups?api-version=2020-10-01`)
+      .then(response => {
+        this.info = response.data.bpi
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  },
+  methods: {
+    createOrUpdateEnvironment() {
+      console.log("create env")
+    }
   }
 }
 </script>
